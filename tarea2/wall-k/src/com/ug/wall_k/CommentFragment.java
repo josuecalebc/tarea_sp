0f822403c3129dab27ac8485def1b0201092c155
josuecalebc <josuecaleb@gmail.com>
2014-02-18 22:35:41 -0600

    tarea 2

---
+++ tarea2/wall-k/src/com/ug/wall_k/CommentFragment.java
@@ -0,0 +1,7 @@
+package com.ug.wall_k;
+
+import android.support.v4.app.Fragment;
+
+public class CommentFragment extends Fragment {
+
+}
---
+++ tarea2/wall-k/src/com/ug/wall_k/FotoActivity.java
@@ -0,0 +1,22 @@
+package com.ug.wall_k;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class FotoActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_foto);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.foto, menu);
+		return true;
+	}
+
+}
